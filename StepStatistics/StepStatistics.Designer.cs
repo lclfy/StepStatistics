7def37a62e97418fe1de67f0933d8bff10503f7f
Sicong Luo <lclfy@icloud.com>
2021-10-03 20:17:44 +0800

    Initial commit

---
+++ StepStatistics/StepStatistics.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace StepStatistics
+{
+    partial class StepStatistics
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(88, 210);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(650, 700);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(750, 210);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(294, 700);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 966);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 966);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "写入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(716, 106);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 35);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "X.X";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(458, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "填写日期(与表头对应)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 182);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "粘贴文本";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 1056);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 24);
+            this.label4.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(88, 98);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "选择图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // StepStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 1178);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "StepStatistics";
+            this.Text = "步数统计";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ StepStatistics/StepStatistics.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using CCWin;
+using NPOI;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace StepStatistics
+{
+
+
+    public partial class StepStatistics : Skin_Mac
+    {
+
+        List<DataUnit> AllDatas;
+        IWorkbook WorkBook;
+        string TextData;
+        string date;
+        string FileName;
+        public StepStatistics()
+        {
+            InitializeComponent();
+        }
+
+        private bool ImportFiles(int type)
+        {
+            //0为excel 1为图片
+            if(type == 0)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();   //显示选择文件对话框 
+                openFileDialog1.Multiselect = false;
+                openFileDialog1.Filter = "Excel 文件 |*.xlsx;*.xls";
+                //openFileDialog1.InitialDirectory = Application.StartupPath + "\\时刻表\\";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+                IWorkbook workBook;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    String fileNames = "已选择：";
+                    string fileName = openFileDialog1.FileName;
+                    {
+                        try
+                        {
+                            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                            if (fileName.IndexOf(".xlsx") > 0) // 2007版本  
+                            {
+                                try
+                                {
+                                    workBook = new XSSFWorkbook(fileStream);  //xlsx数据读入workbook  
+                                    WorkBook = workBook;
+                                    FileName = fileName;
+                                    if (TextData.Length != 0)
+                                    {
+                                        button2.Enabled = true;
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    MessageBox.Show("读取表格时出现错误\n" + fileName + "\n错误内容：" + e.ToString().Split('在')[0], "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                }
+
+                            }
+                            else if (fileName.IndexOf(".xls") > 0) // 2003版本  
+                            {
+                                // try
+                                {
+                                    workBook = new HSSFWorkbook(fileStream);  //xls数据读入workbook  
+                                    WorkBook = workBook;
+                                    FileName = fileName;
+                                    if (TextData.Length != 0)
+                                    {
+                                        button2.Enabled = true;
+                                    }
+                                }
+                                // catch (Exception e)
+                                {
+                                    //  MessageBox.Show("读取问题查询表时出现错误\n" + fileName + "\n错误内容：" + e.ToString().Split('在')[0], "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                    // return false;
+                                }
+                            }
+                            fileStream.Close();
+                            fileStream.Close();
+                        }
+                        catch (IOException)
+                        {
+                             MessageBox.Show("读取表格时出现错误\n" + fileName, "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                    fileNames = fileNames + fileName;
+                    label4.Text = fileNames;
+                }
+            }
+            else if(type == 1)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();   //显示选择文件对话框 
+                openFileDialog1.Multiselect = false;
+                openFileDialog1.Filter = "图片 |*.jpg;*.png";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string file = openFileDialog1.FileName;
+                }
+
+
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllDatas = new List<DataUnit>();
+            TextData = "";
+            date = "";
+            button2.Enabled = false;
+            FileName = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ImportFiles(0);
+        }
+
+        private void getData()
+        {
+            removeUnuseableWords();
+            List<DataUnit> _allDU = new List<DataUnit>();
+            string[] splitedText = TextData.Split('\n');
+            int allCounter = 0;
+            int nameCounter = 0;
+            int stepCounter = 0;
+            for(int i = 0; i < splitedText.Length; i++)
+            {
+                string _tempText = splitedText[i].Trim().Replace(" ","").Replace(" ","");
+                //如果是空的就跳过
+                if(_tempText.Trim().Length == 0)
+                {
+                    continue;
+                }
+                //分三种情况，只数字，只汉字，汉字数字叠加
+                //遇到哪一种，总数+1，该种类计数+1，填入既定位置
+                //汉字数字，直接添加一整个
+                if(Regex.IsMatch(_tempText, @"^[\u4E00-\u9FA50-9]+$") &&
+                    !Regex.IsMatch(_tempText, @"^[\u4E00-\u9FA5]*$")&&
+                    !Regex.IsMatch(_tempText, @"^[0-9]*$"))
+                {
+                    //取汉字部分
+                    string _name = Regex.Replace(_tempText, @"[0-9]", ""); //只留汉字
+                    //取数字部分
+                    string _step = Regex.Replace(_tempText, @"[^0-9]", ""); //
+                    DataUnit _du = new DataUnit(_name, _step);
+                    _allDU.Add(_du);
+                    allCounter++;
+                    nameCounter++;
+                    stepCounter++;
+                }
+                //汉字，添加姓名部分
+                else if (Regex.IsMatch(_tempText, @"^[\u4E00-\u9FA5]*$"))
+                {
+                    string _name = _tempText;
+                    //有多余的
+                    if(nameCounter < stepCounter && nameCounter < allCounter)
+                    {
+                        _allDU[nameCounter].name = _name;
+                    }
+                    //没有多余的，添加一个新的
+                    if(nameCounter == allCounter)
+                    {
+                        DataUnit _du = new DataUnit(_name, "");
+                        _allDU.Add(_du);
+                        allCounter++;
+                    }
+                    nameCounter++;
+                }
+                //数字，添加步数部分
+                else if(Regex.IsMatch(_tempText, @"^[0-9]*$"))
+                {
+                    string _step = _tempText;
+                    //有多余的
+                    if (stepCounter < nameCounter && stepCounter < allCounter)
+                    {
+                        _allDU[stepCounter].steps = _step;
+                    }
+                    else if (stepCounter == allCounter)
+                    {
+                        DataUnit _du = new DataUnit("", _step);
+                        _allDU.Add(_du);
+                        allCounter++;
+                    }
+                    stepCounter++;
+
+                }
+            }
+            AllDatas = _allDU;
+            string _showntext = "共计"+_allDU.Count+"人：";
+            foreach (DataUnit _tempdu in _allDU)
+            {
+                _showntext = _showntext + "\n" + _tempdu.name + "-" + _tempdu.steps;
+            }
+            richTextBox2.Text = _showntext;
+        }
+
+        private void removeUnuseableWords()
+        {
+            TextData = TextData.Replace("郑容段列车长", "");
+            TextData = TextData.Replace("郑客段列车长王凯", "王凯男");
+            TextData = TextData.Replace("郑客段列车长", "");
+            TextData = TextData.Replace("郑客", "");
+            TextData = TextData.Replace(".", "");
+            TextData = TextData.Replace("A平安银行党志诚", "党志诚");
+            TextData = TextData.Replace("棼", "梦");
+            TextData = TextData.Replace("鵾", "鹍");
+            TextData = TextData.Replace("𡝭", "娟");
+            TextData = TextData.Replace("𡈼", "王");
+            TextData = TextData.Replace("高铁", "");
+            TextData = TextData.Replace("一队", "");
+            TextData = TextData.Replace("京武", "");
+            TextData = TextData.Replace("京郑", "");
+            TextData = TextData.Replace("呼和", "");
+            TextData = TextData.Replace("京商", "");
+            TextData = TextData.Replace("京南", "");
+            TextData = TextData.Replace("京西", "");
+            TextData = TextData.Replace("一组", "");
+            TextData = TextData.Replace("1组", "");
+            TextData = TextData.Replace("二组", "");
+            TextData = TextData.Replace("2组", "");
+            TextData = TextData.Replace("三组", "");
+            TextData = TextData.Replace("3组", "");
+            TextData = TextData.Replace("四组", "");
+            TextData = TextData.Replace("4组", "");
+            TextData = TextData.Replace("五组", "");
+            TextData = TextData.Replace("5组", "");
+            TextData = TextData.Replace("六组", "");
+            TextData = TextData.Replace("6组", "");
+            TextData = TextData.Replace("悅", "悦");
+            TextData = TextData.Replace("雲", "云");
+
+            //去手机号
+            TextData = Regex.Replace(TextData, @"\d{11}", "");
+
+        }
+
+        //写入excel文件
+        private void writeData()
+        {
+            IWorkbook workbook = WorkBook;
+            ISheet sheet = workbook.GetSheetAt(0);
+            int dateColumn = 0;
+            //在第一行找日期
+            if(sheet.GetRow(0) == null || date.Length == 0)
+            {
+                MessageBox.Show("未填写日期或未在表格第一行找到日期");
+                return;
+            }
+            IRow _rowdate = sheet.GetRow(0);
+            bool hasGotDate = false;
+            for(; dateColumn <= _rowdate.LastCellNum; dateColumn++)
+            {
+                if (_rowdate.GetCell(dateColumn) != null)
+                {
+                    if(_rowdate.GetCell(dateColumn).ToString().Length != 0)
+                    {
+                        if (_rowdate.GetCell(dateColumn).ToString().Trim().Equals(date))
+                        {
+                            //找到了，跳出
+                            hasGotDate = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!hasGotDate)
+            {
+                MessageBox.Show("未填写日期或未在表格第一行找到日期");
+                return;
+            }
+            //没找到的，在后面添加
+            List<DataUnit> _hasNotFoundedDU = new List<DataUnit>();
+            List<DataUnit> _allDU = AllDatas;
+            //第一列是姓名，用姓名库在表里匹配一下，没有的话拎出来新建
+            //表格里的所有名字
+            List<DataUnit> _allNames = new List<DataUnit>();
+            for (int i = 1;i<= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if(row == null)
+                {
+                    continue;
+                }
+                if(row.GetCell(0) != null)
+                {
+                    if(row.GetCell(0).ToString().Length != 0)
+                        {
+                        DataUnit _tempDU = new DataUnit(row.GetCell(0).ToString().Trim().Replace(" ", ""), "", i);
+                            _allNames.Add(_tempDU);
+                        }
+                }
+            }
+            //匹配一下，没有的新建，有的写上
+            foreach (DataUnit _du in AllDatas)
+            {
+                bool hasGotLineup = false;
+                for (int _s = 0; _s < _allNames.Count; _s++)
+                {
+                    if (_du.name.Trim().Equals(_allNames[_s].name.Replace("（","").Replace("）","")))
+                    {
+                        if(sheet.GetRow(_allNames[_s].indexAt)== null)
+                        {
+                            sheet.CreateRow(_allNames[_s].indexAt);
+                        }
+                        IRow _tempRow = sheet.GetRow(_allNames[_s].indexAt);
+                        if(_tempRow.GetCell(dateColumn) == null)
+                        {
+                            _tempRow.CreateCell(dateColumn);
+                        }
+                        _tempRow.GetCell(dateColumn).SetCellValue(_du.steps);
+                        hasGotLineup = true;
+                        break;
+                    }
+                }
+                if(hasGotLineup == false)
+                {
+                    _hasNotFoundedDU.Add(_du);
+                }
+            }
+            //没有的新建
+            int lastRow = -1;
+            //找最后一行(空的第一行)
+            for (int ij=1;ij<sheet.LastRowNum;ij++)
+            {
+                if(sheet.GetRow(ij) == null)
+                {
+                    lastRow = ij;
+                    break;
+                }
+                if(sheet.GetRow(ij).GetCell(0) == null)
+                {
+                    lastRow = ij;
+                    break;
+                }
+                if (sheet.GetRow(ij).GetCell(0).ToString().Length == 0)
+                {
+                    lastRow = ij;
+                    break;
+                }
+            }
+            if(lastRow == -1)
+            {
+                lastRow = sheet.LastRowNum;
+            }
+            for (int counter = 0; counter < _hasNotFoundedDU.Count; counter++)
+            {
+                if(sheet.GetRow(lastRow + counter) == null)
+                {
+                    sheet.CreateRow(lastRow + counter);
+                }
+                IRow _tempRow = sheet.GetRow(lastRow + counter);
+                if (_tempRow.GetCell(0) == null)
+                {
+                    _tempRow.CreateCell(0);
+                }
+                _tempRow.GetCell(0).SetCellValue(_hasNotFoundedDU[counter].name);
+                if(_tempRow.GetCell(dateColumn) == null)
+                {
+                    _tempRow.CreateCell(dateColumn);
+                }
+                _tempRow.GetCell(dateColumn).SetCellValue(_hasNotFoundedDU[counter].steps);
+            }
+            /*重新修改文件指定单元格样式*/
+            FileStream fs1 = File.OpenWrite(FileName);
+            workbook.Write(fs1);
+            fs1.Close();
+            workbook.Close();
+            MessageBox.Show("写入完成", "提示", MessageBoxButtons.OK);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextData = richTextBox1.Text;
+            if (TextData.Length != 0 && TextData != null)
+            {
+                getData();
+                if(WorkBook != null)
+                {
+                    button2.Enabled = true;
+                }
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeData();
+        }
+
+        //选择图片
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ImportFiles(1);
+        }
+
+        //输入日期
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            date = textBox1.Text;
+        }
+    }
+
+    public class DataUnit
+    {
+        public string name { get; set; }
+        public string steps { get; set; }
+        public int indexAt { get; set; }
+
+        public DataUnit(string _name, string _steps,int _indexAt = -1)
+        {
+            name = _name;
+            steps = _steps;
+            indexAt = _indexAt;
+        }
+
+    }
+}
